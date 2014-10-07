5e1f37d388344e06515f4e05081c259af764a963
Pedro Bellesa <bspedropaulo@myseneca.ca>
2014-10-06 21:28:04 -0400

    Changes to javascript object and description.

--- js/angular-mains.js
+++ js/angular-mains.js
@@ -11,7 +11,7 @@
             {name: "Ponte Vasco da Gama", description: "Lisboa, Portugal", image: "portfolio-vg1.jpg", position: "38.758156, -9.037735", target: "vgm"},
             {name: "Terceira Ponte", description: "Vitória - ES, Brasil", image: "portfolio-t1.jpg", position: "-20.325067, -40.283130", target: "tc1"},
             {name: "Metro SP - Linha 5", description: "São Paulo - SP, Brasil", image: "portfolio-metro.jpg", position: "-23.659790, -46.768897", target: "met"},
-            {name: "Ponte Florentino Avidos", description: "Colatina - ES, Brasil", image: "portfolio-colatina.jpg", position: "-19.531184, -40.631124", target: "col"},
+            {name: "Segunda Ponte Colatina", description: "Colatina - ES, Brasil", image: "portfolio-colatina.jpg", position: "-19.531184, -40.631124", target: "col"},
             {name: "Linha Vermelha", description: "Rio de Janeiro - RJ, Brasil", image: "portfolio-linhavermelha.jpg", position: "-22.805602, -43.295304", target: "lvr"},
             {name: "Mercado Abastecedor da Região de Lisboa", description: "São Julião do Tojal - Portugal", image: "portfolio-marl.jpg", position: "38.867432, -9.106789", target: "mrl"}
         ];
